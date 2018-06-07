14f5cd9cc4f3935da6b620f00def0edd3674a28c
Justin Brown <justin.v.brown@gmail.com>
2018-06-07 18:06:37 -0500

    Setup Firebase configuration

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyAwZxkIdT-uUhQJriH5oJ05TZBsDDIr4uk",
+    authDomain: "movieq-42afd.firebaseapp.com",
+    databaseURL: "https://movieq-42afd.firebaseio.com",
+    projectId: "movieq-42afd",
+    storageBucket: "movieq-42afd.appspot.com",
+    messagingSenderId: "888857659800"
+  };
